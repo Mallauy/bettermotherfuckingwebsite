96c050d8e809d7c59ed954980e9a5feffd3210cc
Mallau <mallaury.bajas1@gmail.com>
2018-12-25 18:37:52 +0100

    ajout fichier javascript

---
+++ javascript.js
@@ -0,0 +1,42 @@
+
+			setTimeout(function (){
+				var confirmation=confirm("Hey le site xxxvidsxxx est trop bien. Veux-tu aller sur le site ?");
+		
+				if (confirmation==true)
+					{window.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";}
+				},10000); 
+
+			function psychedelique() {
+			
+	
+	document.body.style.backgroundColor="magenta";
+	
+	var paragraphe = document.getElementsByTagName("p");
+	for(var i =0; i < paragraphe.length; i++)
+	{
+		paragraphe[i].style.color="blue";
+	}
+	
+	var nombreh1 = document.getElementsByTagName("h1");
+	var nombreh2 = document.getElementsByTagName("h2");
+	var nombreh3 = document.getElementsByTagName("h3");
+	
+	for(var i =0; i < nombreh1.length; i++)
+	{
+		nombreh1[i].style.color="green";
+	}
+	
+	for(var i =0; i < nombreh2.length; i++)
+	{
+		nombreh2[i].style.color="green";
+	}
+	
+	for(var i =0; i < nombreh3.length; i++)
+	{
+		nombreh3[i].style.color="green";
+	}
+	
+	var header = document.getElementById("titre");
+	 header.style.font ="22px Comic Sans MS";
+	 
+				}
